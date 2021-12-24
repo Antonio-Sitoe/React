ad7d9c75fd347f53b62f5b0905b26ecc2ada99b9
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2021-12-24 14:19:36 +0200

    3

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import Exemplo from './Exemplo';
 
-const App = () => <div></div>;
+function reducer(state, action) {
+  switch (action) {
+    case 'aumentar':
+      return state + 1;
+    case 'diminuir':
+      return state - 1;
+    default:
+      return new Error();
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = React.useReducer(reducer, 0);
+
+  function aumentar() {
+    dispatch('aumentar');
+  }
+  function diminuir() {
+    dispatch('diminuir');
+  }
+  return (
+    <div>
+      <button onClick={aumentar}>+</button>
+      <button onClick={diminuir}>+</button>
+      <p>{state}</p>
+      <Exemplo/>
+    </div>
+  );
+};
 
 export default App;
