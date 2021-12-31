779baa850c52a4200384648569521caf54ce16d7
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2021-12-31 22:59:03 +0200

    app

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import Exemplo from './Exemplo';
-
-function reducer(state, action) {
-  switch (action) {
-    case 'aumentar':
-      return state + 1;
-    case 'diminuir':
-      return state - 1;
-    default:
-      return new Error();
-  }
-}
+import Slide from './Slide';
 
 const App = () => {
-  const [state, dispatch] = React.useReducer(reducer, 0);
-
-  function aumentar() {
-    dispatch('aumentar');
-  }
-  function diminuir() {
-    dispatch('diminuir');
-  }
+  const slides = [
+    {
+      id: '1',
+      text: 'slide 1',
+    },
+    {
+      id: '2',
+      text: 'slide 2',
+    },
+    {
+      id: '3',
+      text: 'slide 3',
+    },
+  ];
   return (
     <div>
-      <button onClick={aumentar}>+</button>
-      <button onClick={diminuir}>+</button>
-      <p>{state}</p>
-      <Exemplo/>
+      <Slide slides={slides} />
     </div>
   );
 };
