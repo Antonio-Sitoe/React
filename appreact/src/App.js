360c560948779385d029b97606346d498f54f1f9
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-01-17 16:11:19 +0200

    update

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import Slide from './Slide';
+import './index.css';
+import commerce from './lib/commerce';
 
 const App = () => {
-  const slides = [
-    {
-      id: '1',
-      text: 'slide 1',
-    },
-    {
-      id: '2',
-      text: 'slide 2',
-    },
-    {
-      id: '3',
-      text: 'slide 3',
-    },
-  ];
+  const [produts, setProduts] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  async function fetchProducts() {
+    setLoading(true);
+    const response = await commerce.products.list();
+    setProduts(response.data);
+    console.log(response.data[0]);
+    setLoading(false);
+  }
+
+  React.useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  if (loading) return <p className='content'>...loading</p>;
   return (
     <div>
-      <Slide slides={slides} />
+      <div className='content'>
+        {produts &&
+          produts.map(({ id, name, assets, price, description }) => {
+            return (
+              <div key={id}>
+                <img src={assets[0] && assets[0].url} alt='' />
+                <h3>{name}</h3>
+                {description}
+                <p>{price.formatted_with_symbol}</p>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
