a5629d8f075e80d0e6bb8f70bf33af7682c9aadb
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-02-26 10:07:17 +0200

    new

--- appreact/src/App.jsx
+++ appreact/src/App.jsx
@@ -1,56 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Content from './components/Content/Content';
-import Header from './components/Header/Header';
-import useLocalStorage from './components/hooks/uselocalstorage';
-import List from './components/List/List';
-import PageError from './components/PageError/PageError';
-import { GlobalStyle } from './styles/GlobalStyle';
-function App() {
-  const [menu, setMenu] = React.useState(true);
-  const {theme, setTheme} = useLocalStorage('theme', false);
 
-  function openMenu() {
-    setMenu(true);
-  }
-  function closeMenu() {
-    setMenu(false);
-  }
+const App = () => {
+  const [count, setCount] = React.useState(0);
+  
   React.useEffect(() => {
-    if (window.innerWidth < 700) {
-      closeMenu();
-    }
-  }, []);
-
-  React.useEffect(() => {
-    function onResize() {
-      if (window.innerWidth > 700) {
-        openMenu();
-      } else {
-        closeMenu();
-      }
-    }
-    window.addEventListener('resize', onResize);
-    return () => window.removeEventListener('resize', onResize);
+    console.log(count);
+    window.document.title = count;
   }, []);
 
   return (
-    <BrowserRouter>
-      <Header openMenu={openMenu} setTheme={setTheme}  theme={theme} />
-      <div className='container'>
-        <List closeMenu={closeMenu} menu={menu} theme={theme} />
-        <Routes>
-          <Route path='' element={<Content closeMenu={closeMenu} />} />
-          <Route
-            path='docs/:route/*'
-            element={<Content closeMenu={closeMenu} />}
-          />
-          <Route path='*' element={<PageError />} />
-        </Routes>
-      </div>
-      <GlobalStyle theme={theme} />
-    </BrowserRouter>
+    <div className='container'>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </div>
   );
-}
+};
 
 export default App;
--- appreact/src/components/PageError/PageError.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PageError = () => {
-  return <div>erro</div>;
-};
-
-export default PageError;
